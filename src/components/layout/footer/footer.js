66e90447cd2f290fcdfd5a666f9bff397ffde0d2
Margarita Yakubovskaya <101704236+yamara28@users.noreply.github.com>
2023-05-09 13:47:32 +0200

    fix style

--- src/components/layout/footer/footer.js
+++ src/components/layout/footer/footer.js
@@ -6,7 +6,8 @@ import Logo from "../../ui/logo/logo";
 const StyledFooter = styled.footer`
     display: flex;
     max-width: 1280px;
-    padding: 10px 90px;`;
+    padding: 10px 90px;
+`;
 
 export default function Footer () {
     return (
